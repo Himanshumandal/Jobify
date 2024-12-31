71fc242481c45cc52ddf662520e6371ce8472775
Himanshu Mandal <113971156+Himanshumandal@users.noreply.github.com>
2024-12-31 22:13:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,32 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { visualizer } from "rollup-plugin-visualizer";
+import viteImagemin from 'vite-plugin-imagemin';
+
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    visualizer({
+      open: true, // Automatically opens the report in your browser
+    }),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 7 },
+      mozjpeg: { quality: 80 },
+      pngquant: { quality: [0.6, 0.8] },
+      svgo: {},
+    }),
+  ],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Split common libraries
+          ui: ['./src/components/UI'],   // Split UI components
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
