de4f04def14903fb89c677351b56132693f74c3c
Himanshu Mandal <113971156+Himanshumandal@users.noreply.github.com>
2024-12-31 22:17:39 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,11 @@
-import path from "path"
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
-import { visualizer } from "rollup-plugin-visualizer";
-import viteImagemin from 'vite-plugin-imagemin';
-
-
-export default defineConfig({
-  plugins: [react(),
-    visualizer({
-      open: true, // Automatically opens the report in your browser
-    }),
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7 },
-      mozjpeg: { quality: 80 },
-      pngquant: { quality: [0.6, 0.8] },
-      svgo: {},
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'], // Split common libraries
-          ui: ['./src/components/UI'],   // Split UI components
-        },
-      },
-    },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+error during build:
+Error [ERR_MODULE_NOT_FOUND]: Cannot find package 'rollup-plugin-visualizer' imported from /vercel/path0/vite.config.js.timestamp-1735663578879-f917cb53f4b17.mjs
+    at packageResolve (node:internal/modules/esm/resolve:857:9)
+    at moduleResolve (node:internal/modules/esm/resolve:926:18)
+    at defaultResolve (node:internal/modules/esm/resolve:1056:11)
+    at ModuleLoader.defaultResolve (node:internal/modules/esm/loader:654:12)
+    at #cachedDefaultResolve (node:internal/modules/esm/loader:603:25)
+    at ModuleLoader.resolve (node:internal/modules/esm/loader:586:38)
+    at ModuleLoader.getModuleJobForImport (node:internal/modules/esm/loader:242:38)
+    at ModuleJob._link (node:internal/modules/esm/module_job:135:49)
+Error: Command "npm run build" exited with 1
